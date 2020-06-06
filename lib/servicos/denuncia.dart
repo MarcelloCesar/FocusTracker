61744e48f6abca6d3b0b9e6da66fa04c2f61a498
brunosilva98 <brunops98@hotmail.com>
2020-06-06 19:32:40 -0300

    ajuste nomes

--- lib/servicos/denuncia.dart
+++ lib/servicos/denuncia.dart
@@ -6,11 +6,11 @@ import 'dart:convert';
 Future<int> realizaDenuncia(String cep, String observacao, String coordenadas, String tipo, String token) async {
   String url = Constantes.URL_API + Constantes.ENDPOINT_DENUNCIA;
   final response = await http.post(url, body: jsonEncode(<String, String> {
-    'coord'   : coordenadas,
-    'tipo'    : tipo,
-    'cep'     : cep,
-    'observ'  : observacao,
-    'token'   : token,
+    'coordenadas' : coordenadas,
+    'tipo'        : tipo,
+    'cep'         : cep,
+    'observacao'  : observacao,
+    'token'       : token,
   })
   );
 
